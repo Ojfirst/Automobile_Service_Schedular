537ca261e15cdb37704fe0e8debd9986424eebdd
Simon <omojide.o.j@gmail.com>
2025-11-14 10:19:00 +0100

    Delete prisma-db.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+
+const seed = async () => {
+	const serviceCount = await prisma.service.count();
+	if (serviceCount === 0) {
+		await prisma.service.createMany({
+			data: [
+				{
+					name: 'Oil Change',
+					description: 'Standard oil and filter change',
+					duration: 30,
+					price: 49.99,
+				},
+				{
+					name: 'Tire Rotation',
+					description: 'Rotate tires and check pressure',
+					duration: 45,
+					price: 29.99,
+				},
+				{
+					name: 'Brake Inspection',
+					description: 'Complete brake system inspection',
+					duration: 60,
+					price: 39.99,
+				},
+			],
+		});
+	}
+};
+
+seed().then(() => prisma.$disconnect());
+
+export const generateService = async () => {
+	return prisma.service.findMany();
+};
