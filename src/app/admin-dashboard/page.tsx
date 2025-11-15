af6cf5ab0a9adb89edcbc8d746bf96e7509c45fd
Simon <omojide.o.j@gmail.com>
2025-11-15 13:13:44 +0100

    Initialise role base access control

---
+++ src/app/admin-dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const AdminDashboardPage = () => {
+  return (
+    <Fragment>
+      <h1>Admin Dashboard</h1>
+
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum animi qui quas blanditiis voluptatem officiis incidunt totam cumque, dolor quo.</div>
+    </Fragment>
+  )
+}
+
+export default AdminDashboardPage
