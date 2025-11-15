b01b4e6f494387a7942487b79105ffc0f6e56528
Simon <omojide.o.j@gmail.com>
2025-11-15 12:30:09 +0100

    Add sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,10 @@
 
-import { SignUp } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs';
 
 
 const SignUpPage = () => {
   return <div className='flex justify-center items-center py-4'>
-    <SignUp />
+    <SignIn />
   </div>
 }
 
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignUp } from '@clerk/nextjs';
+
+
+const SignInPage = () => {
+  return <div className='flex justify-center items-center py-4'>
+    <SignUp />
+  </div>
+}
+
+export default SignInPage;
