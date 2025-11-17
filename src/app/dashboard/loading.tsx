e934737a841e27e5619951ccbd263d68ff56fcf9
Simon <omojide.o.j@gmail.com>
2025-11-17 08:19:23 +0100

    Add dashboard loading state

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <h1 className='text-4xl flex justify-center items-center h-screen'>loading...</h1>
+  )
+}
+
+
+export default Loading;
